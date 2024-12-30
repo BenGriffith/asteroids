06bb29b7d252ff5819aae37e03a40ea19771650c
Ben Griffith <bengriffith@outlook.com>
2024-12-29 21:08:59 -0600

    implement groups

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
-        player.update(dt)
-        player.draw(screen=screen)
+        for updatable_player in updatable:
+            updatable_player.update(dt)
+        for drawable_player in drawable:
+            drawable_player.draw(screen=screen)
         time_passed = clock.tick(60)
         dt = time_passed / 1000
         pygame.display.flip()
